8a70250cc0940691786cebc19b1c2167d4206fe1
Luke Arms <luke@linacreative.com>
2023-08-29 17:15:38 +1000

    pretty-php 0.4.21
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pretty-php.rb
+++ Formula/pretty-php.rb
@@ -1,8 +1,8 @@
 class PrettyPhp < Formula
   desc "Opinionated code formatter for PHP"
   homepage "https://github.com/lkrms/pretty-php"
-  url "https://github.com/lkrms/pretty-php/releases/download/v0.4.20/pretty-php.phar"
-  sha256 "7d6b1c9f376fdee0825f0f360cc79fa9b9ce17b6add8d1eac68b83813650b361"
+  url "https://github.com/lkrms/pretty-php/releases/download/v0.4.21/pretty-php.phar"
+  sha256 "f7238f9dec4cceee00a9d000fcabff693805397068ec51f49cf6ed098a46e1d2"
   license "MIT"
 
   depends_on "php"
