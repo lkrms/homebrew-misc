9e6a412ac88d44c6217885e8d31ada9b58160937
Luke Arms <luke@linacreative.com>
2023-09-07 11:07:02 +1000

    Update pretty-php description

--- Formula/pretty-php.rb
+++ Formula/pretty-php.rb
@@ -1,5 +1,5 @@
 class PrettyPhp < Formula
-  desc "Opinionated code formatter for PHP"
+  desc "Opinionated PHP code formatter"
   homepage "https://github.com/lkrms/pretty-php"
   url "https://github.com/lkrms/pretty-php/releases/download/v0.4.23/pretty-php.phar"
   sha256 "8b6acecc65fd7b95da0dc7d882fa63c9e882e389f32e154c29ec75f8ec6f9aee"
