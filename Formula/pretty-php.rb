c97b78e7ae574b06c46473d7cec68fb40c419c9c
Luke Arms <luke@linacreative.com>
2023-09-14 20:21:41 +1000

    Fix pretty-php man file build

--- Formula/pretty-php.rb
+++ Formula/pretty-php.rb
@@ -4,13 +4,16 @@ class PrettyPhp < Formula
   url "https://github.com/lkrms/pretty-php/releases/download/v0.4.26/pretty-php.phar"
   sha256 "5c28dc20c476f78af4a80dd0f1f987467e86df4a850c7708c2be0faba3f6d4bf"
   license "MIT"
+  revision 1
 
   depends_on "pandoc" => :build
   depends_on "php"
 
   def install
-    bin.install "pretty-php.phar" => "pretty-php"
-    system "#{bin}/pretty-php _man | #{Formula["pandoc"].bin}/pandoc --standalone --to man -o pretty-php.1"
+    mv "pretty-php.phar", "pretty-php"
+    system "#{Formula["php"].bin}/php -ddisplay_errors=stderr pretty-php _man | #{Formula["pandoc"].bin}/pandoc --standalone --to man -o pretty-php.1"
+
+    bin.install "pretty-php"
     man1.install "pretty-php.1"
   end
 
