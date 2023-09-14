5f5f6b4d318c2c52899dcae76f0f68c0a8ef8d1c
Luke Arms <luke@linacreative.com>
2023-09-14 15:46:24 +1000

    Add pretty-php man file to formula

--- Formula/pretty-php.rb
+++ Formula/pretty-php.rb
@@ -5,10 +5,13 @@ class PrettyPhp < Formula
   sha256 "537d32258716abacd4fc50c844e4414e51d325bfaf391e5496b706e952739169"
   license "MIT"
 
+  depends_on "pandoc" => :build
   depends_on "php"
 
   def install
     bin.install "pretty-php.phar" => "pretty-php"
+    system "#{bin}/pretty-php _man | #{Formula["pandoc"].bin}/pandoc --standalone --to man -o pretty-php.1"
+    man1.install "pretty-php.1"
   end
 
   test do
